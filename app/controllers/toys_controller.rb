68bb5c22864c04c8d7a34a84758114f707ff45f7
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-09-23 11:14:25 -0700

    Done.

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,18 +7,19 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+   
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy.update(toy_params) 
     render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
@@ -29,4 +30,7 @@ class ToysController < ApplicationController
     params.permit(:name, :image, :likes)
   end
 
+  def find_toy
+    Toy.find(params[:id])
+  end
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,3 +1,2 @@
 class Toy < ApplicationRecord
-
 end
